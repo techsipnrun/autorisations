6e29d9e636fad184e418a5aa6b06daec7ed2021a
CALU Louis <louis.calu@reunion-parcnational.fr>
2026-03-06 11:15:50 +0400

    Comment fonction

--- autorisations/src/instruction/utils/files_utils.py
+++ autorisations/src/instruction/utils/files_utils.py
@@ -15,33 +15,33 @@ logger = logging.getLogger("ORM_DJANGO")
 
 
 
-def save_if_not_exists(request, dossier, file_obj, dest_path, fail_if_exists=True):
-    """
-    Sauvegarde un fichier sur le NAS uniquement s’il n’existe pas déjà.
-    Renvoie :
-        - None   → OK
-        - redirect(...) → en cas d’erreur, géré proprement via redirect_error
-    """
+# def save_if_not_exists(request, dossier, file_obj, dest_path, fail_if_exists=True):
+#     """
+#     Sauvegarde un fichier sur le NAS uniquement s’il n’existe pas déjà.
+#     Renvoie :
+#         - None   → OK
+#         - redirect(...) → en cas d’erreur, géré proprement via redirect_error
+#     """
 
-    # Le dossier_path est juste utile pour les logs
-    dossier_path = ntpath.dirname(dest_path).replace("\\", "/")
+#     # Le dossier_path est juste utile pour les logs
+#     dossier_path = ntpath.dirname(dest_path).replace("\\", "/")
 
-    # Déjà présent → OK
-    if smbclient.path.exists(dest_path) and fail_if_exists :
-        logger.info(f"[DOSSIER {dossier.numero}] Pas d'écriture par {request.user} car un fichier du même nom existe déjà : {_normalize_unc_path(dossier_path)}")
-        return redirect_error(request, f"❌ Impossible d'enregistrer le fichier {file_obj.name} : Un fichier du même nom existe déjà à l'emplacement {_normalize_unc_path(dossier_path)}")
+#     # Déjà présent → OK
+#     if smbclient.path.exists(dest_path) and fail_if_exists :
+#         logger.info(f"[DOSSIER {dossier.numero}] Pas d'écriture par {request.user} car un fichier du même nom existe déjà : {_normalize_unc_path(dossier_path)}")
+#         return redirect_error(request, f"❌ Impossible d'enregistrer le fichier {file_obj.name} : Un fichier du même nom existe déjà à l'emplacement {_normalize_unc_path(dossier_path)}")
 
-    try:
-        if not ecrire_file_sur_nas(file_obj, dest_path):
-            raise Exception(f"Échec de l’écriture du fichier {_normalize_unc_path(dossier_path)} sur le NAS par {request.user}.")
+#     try:
+#         if not ecrire_file_sur_nas(file_obj, dest_path):
+#             raise Exception(f"Échec de l’écriture du fichier {_normalize_unc_path(dossier_path)} sur le NAS par {request.user}.")
 
-        logger.info(f"[DOSSIER {dossier.numero}] '{file_obj.name}' enregistré dans le Dossier Work par {request.user}.")
+#         logger.info(f"[DOSSIER {dossier.numero}] '{file_obj.name}' enregistré dans le Dossier Work par {request.user}.")
 
-    except Exception as e:
-        logger.error(f"[DOSSIER {dossier.numero}] Erreur lors de l'écriture du fichier {_normalize_unc_path(dossier_path)} sur le NAS par {request.user} : {e}")
-        return redirect_error(request, f"❌ Impossible d’enregistrer le fichier {file_obj.name}. Contactez le support.")
+#     except Exception as e:
+#         logger.error(f"[DOSSIER {dossier.numero}] Erreur lors de l'écriture du fichier {_normalize_unc_path(dossier_path)} sur le NAS par {request.user} : {e}")
+#         return redirect_error(request, f"❌ Impossible d’enregistrer le fichier {file_obj.name}. Contactez le support.")
 
-    return None
+#     return None
 
 
 def generate_unique_filename(dir_abs_path: str, dir_rel_path: str, base_filename: str):
